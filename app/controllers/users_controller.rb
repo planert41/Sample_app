b125a3cff6abb183dc0dd2de8c60d71ae9608062
Wei Zou <planert41@gmail.com>
2014-02-23 19:07:35 -0600

    Changes

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,6 +1,5 @@
 class UsersController < ApplicationController
-  def new
-  end
+
 
     def show
     @user = User.find(params[:id])
@@ -14,6 +13,7 @@ class UsersController < ApplicationController
   def create
     @user = User.new(user_params)
     if @user.save
+      sign_in @user
       flash[:success] = "Welcome to the Sample App!"
     	redirect_to @user
       # Handle a successful save.
